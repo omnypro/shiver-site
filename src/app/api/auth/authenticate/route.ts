709cedf1516681b15d3cd1bc5f8262a019024e86
Bryan Veloso <bryan@velo.so>
2024-07-07 10:21:32 -0700

    Update scopes.

--- src/app/api/auth/authenticate/route.ts
+++ src/app/api/auth/authenticate/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server';
 export async function GET() {
   const clientId = process.env.AUTH_TWITCH_ID;
   const redirectUri = process.env.NEXT_PUBLIC_AUTH_TWITCH_REDIRECT_URI;
-  const scope = 'user_read';
+  const scope = 'user:read:email user:read:follows user:read:subscriptions';
   const authUrl = `https://id.twitch.tv/oauth2/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code&scope=${scope}`;
   return NextResponse.redirect(authUrl);
 }
