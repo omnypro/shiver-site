4a2648d16b6174e14849bbd19b7016d24d86f72b
Bryan Veloso <bryan@velo.so>
2024-07-07 10:29:33 -0700

    Attempt to fix Safari downloading the auth endpoint.

--- src/app/api/auth/authenticate/route.ts
+++ src/app/api/auth/authenticate/route.ts
@@ -7,3 +7,5 @@ export async function GET() {
   const authUrl = `https://id.twitch.tv/oauth2/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code&scope=${scope}`;
   return NextResponse.redirect(authUrl);
 }
+
+export const runtime = 'edge';
